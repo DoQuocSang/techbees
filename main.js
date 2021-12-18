f9377d5abb0618021d8746b43c8511551f8f98df
DoQuocSang <92617630+DoQuocSang@users.noreply.github.com>
2021-12-18 11:44:44 +0700

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-{
-    let checkbox = document.querySelector('#checkbox');
-    let body = document.querySelector('body')
-    checkbox.onclick = function(){
-        body.classList.toggle('on');
-    }
-}
